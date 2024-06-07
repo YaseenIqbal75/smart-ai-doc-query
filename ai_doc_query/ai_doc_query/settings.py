ec3ee28cfb796bce546b6a04de05db72d5fa0e48
Yaseen Iqbal <yaseen.iqbal@eritheialabs.io>
2024-06-07 16:46:48 +0500

    feat:Database integrated

--- ai_doc_query/ai_doc_query/settings.py
+++ ai_doc_query/ai_doc_query/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,11 +76,14 @@ WSGI_APPLICATION = 'ai_doc_query.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'ai_doc_query',
+        'CLIENT': {
+            'host': 'localhost',  # MongoDB host
+            'port': 27017,  # MongoDB port
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

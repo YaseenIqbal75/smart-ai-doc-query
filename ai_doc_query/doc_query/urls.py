091a64784990460277e7c23522c43d0114d22ccb
Yaseen Iqbal <yaseen.iqbal@eritheialabs.io>
2024-06-12 19:25:47 +0500

    feat: Implemented crud apis for chat

--- ai_doc_query/doc_query/urls.py
+++ ai_doc_query/doc_query/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("user/" , views.UserApis.as_view(), name="create_user"),
-    path("user/<str:id>" , views.UserApis.as_view(), name="delete_update_user")
+    path("user/" , views.UserApis.as_view(), name="create_get_user"),
+    path("user/<str:id>" , views.UserApis.as_view(), name="delete_update_user"),
+    path("chat/", views.ChatApis.as_view(), name= "create_get_chat"),
+    path("chat/<str:id>", views.ChatApis.as_view(), name= "update_delete_chat")
 ]
--- ai_doc_query/doc_query/views.py
+++ ai_doc_query/doc_query/views.py
@@ -11,7 +11,7 @@ from django.utils.decorators import method_decorator
 @method_decorator(csrf_exempt, name='dispatch')
 class UserApis(View):
 
-    def get(self, request, id=None):
+    def get(self,request,id=None):
         if id:
             try:
                 user = User.objects.get(pk=id)
@@ -24,7 +24,7 @@ class UserApis(View):
             except User.DoesNotExist:
                 return JsonResponse({"message": "User does not exist"}, status=404)
             except Exception as e:
-                return JsonResponse({"message": str(e)}, status=500)
+                return JsonResponse({"message": str(e)}, status=400)
         else:
             try:
                 users_list = []
@@ -39,7 +39,7 @@ class UserApis(View):
 
                 return JsonResponse(users_list, safe=False, status=200)
             except Exception as e:
-                return JsonResponse({"message": str(e)}, status=500)
+                return JsonResponse({"message": str(e)}, status=400)
 
     def post(self,request):
         try:
@@ -83,9 +83,9 @@ class UserApis(View):
         except json.JSONDecodeError:
             return JsonResponse({"message": "Invalid JSON"}, status=400)
         except Exception as e:
-            return JsonResponse({"message": str(e)}, status=500)
+            return JsonResponse({"message": str(e)}, status=400)
         
-    def delete(self, request, id):
+    def delete(self,request,id):
         try:
             try:
                 req_user = User.objects.get(pk=id)
@@ -93,7 +93,86 @@ class UserApis(View):
                 return JsonResponse({"message": "User does not exist"}, status=404)
 
             req_user.delete()
-            return JsonResponse({"message": "The user was deleted"}, status=200)
+            return JsonResponse({"message": "The user was deleted"}, status=204)
 
         except Exception as e:
-            return JsonResponse({"message": str(e)}, status=500)
+            return JsonResponse({"message": str(e)}, status=400)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class ChatApis(View):
+    def get(self,request, id=None):
+        if id:
+            try:
+                chat = Chat.objects.get(pk=id)
+                chat_data= {
+                    "id": str(chat.id),
+                    "title" : chat.title,
+                    "creation_timestamp": chat.creation_timestamp,
+                    "owner" : User.objects.get(pk=chat.owner.id).email
+                }
+                return JsonResponse(chat_data, status = 200)
+            except Exception as e:
+                return JsonResponse({"message" : str(e)}, status = 400)
+        else:
+            try:
+                chat_list = []
+                for chat in Chat.objects:
+                    chat_data= {
+                    "id": str(chat.id),
+                    "title" : chat.title,
+                    "creation_timestamp": chat.creation_timestamp,
+                    "owner" : User.objects(pk = chat.owner.id).first().email
+                    }
+                    chat_list.append(chat_data)
+                return JsonResponse(chat_list,safe=False,status = 200)
+            except Exception as e:
+                return JsonResponse({"message" : str(e)}, status = 400)
+
+    def post(self,request):
+        try:
+            data = json.loads(request.body)
+            title = data.get("title")
+            owner = User.objects(email="arslanwaqar421@gmail.com").first() # static user
+
+            if not title or not owner:
+                return JsonResponse({"message" : "Chat title and Owner required"}, status = 400)
+
+            new_chat = Chat(title = title , owner = owner)
+            new_chat.save()
+
+            return JsonResponse({"message" : "Chat created successfully"},status = 201)
+
+        except Exception as e:
+            return JsonResponse({"message" : str(e)}, status = 400)
+
+    def put(self,request,id):
+        try:
+            data = json.loads(request.body)
+            title = data.get("title")
+
+            if not title:
+                return JsonResponse({"message" : "Title is required"}, status= 400)
+
+            try:
+                req_chat = Chat.objects.get(pk=id)
+                req_chat.title = title
+                req_chat.save()
+                return JsonResponse({"message": "Title updated"}, status=200)
+            except DoesNotExist:
+                JsonResponse({"message" : "Chat does not exist"}, status = 404)
+
+        except Exception as e:
+            return JsonResponse({"message" : str(e)}, status = 400)
+
+    def delete(self, request, id):
+        try:
+            try:
+                req_chat = Chat.objects.get(pk=id)
+            except DoesNotExist:
+                return JsonResponse({"message": "Chat does not exist"}, status=404)
+
+            req_chat.delete()
+            return JsonResponse({"message" : "Chat deleted succesfully"}, status=204)
+        except Exception as e:
+            return JsonResponse({"message" : str(e)})

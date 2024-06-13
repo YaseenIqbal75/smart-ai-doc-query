2faa1297f5b7788c4b2acdbfd73a885c613122cc
Yaseen Iqbal <yaseen.iqbal@eritheialabs.io>
2024-06-13 16:33:07 +0500

    feat: Implemented crud apis for message

--- ai_doc_query/doc_query/urls.py
+++ ai_doc_query/doc_query/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("user/" , views.UserApis.as_view(), name="create_get_user"),
     path("user/<str:id>" , views.UserApis.as_view(), name="delete_update_user"),
     path("chat/", views.ChatApis.as_view(), name= "create_get_chat"),
-    path("chat/<str:id>", views.ChatApis.as_view(), name= "update_delete_chat")
+    path("chat/<str:id>", views.ChatApis.as_view(), name= "update_delete_chat"),
+    path("message/", views.MessageApis.as_view(), name="create_get_message"),
+    path("message/<str:id>", views.MessageApis.as_view(),name="update_delete_message")
 ]
--- ai_doc_query/doc_query/views.py
+++ ai_doc_query/doc_query/views.py
@@ -160,7 +160,7 @@ class ChatApis(View):
                 req_chat.save()
                 return JsonResponse({"message": "Title updated"}, status=200)
             except DoesNotExist:
-                JsonResponse({"message" : "Chat does not exist"}, status = 404)
+                return JsonResponse({"message" : "Chat does not exist"}, status = 404)
 
         except Exception as e:
             return JsonResponse({"message" : str(e)}, status = 400)
@@ -176,3 +176,92 @@ class ChatApis(View):
             return JsonResponse({"message" : "Chat deleted succesfully"}, status=204)
         except Exception as e:
             return JsonResponse({"message" : str(e)})
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class MessageApis(View):
+    def get(self, request, id=None):
+        if id:
+            try:
+                req_msg = Message.objects.get(pk=id)
+                msg = {
+                    "id" : str(req_msg.id),
+                    "chat": req_msg.chat.title,
+                    "type": str(req_msg.type)
+                }
+
+                return JsonResponse(msg ,status = 200)
+            except DoesNotExist:
+                return JsonResponse({"message" : "Message does not exist"}, status = 400)
+            except Exception as e:
+                return JsonResponse({"message" : str(e)}, status= 500)
+        else:
+            try:
+                msg_list = []
+
+                for msg in Message.objects:
+                    data = {
+                    "id" : str(msg.id),
+                    "msg_txt" : msg.msg_txt,
+                    "chat": msg.chat.title,
+                    "type":str(msg.type)
+                }
+                    msg_list.append(data)
+
+                return JsonResponse(msg_list, safe=False, status=200)
+            except Exception as e:
+                return  JsonResponse({"message": str(e)}, status=500)
+
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            msg_txt = data.get("msg_txt")
+            type = data.get("type")
+            chat = Chat.objects.get(pk=data.get("chat")) #static chat
+
+            if not msg_txt or not type or not chat:
+                return JsonResponse({"message": "Message text , type and chat are required"}, status =400)
+
+            if type == "bot":
+                type = MessageType.BOT
+            elif type == "user":
+                type = MessageType.USER
+            else:
+                return JsonResponse({"message":"Message type can either be 'user' or 'bot' "},status= 400)
+
+            new_msg = Message(msg_txt=msg_txt , type=type , chat=chat)
+            new_msg.save()
+            return JsonResponse({"message" : "Message created successfully"},status = 201)
+        except DoesNotExist as d:
+            return JsonResponse({"message": str(d)},status = 400)
+        except Exception as e:
+            return JsonResponse({"message": str(e)}, status= 500)
+
+    def put(self, request, id):
+        try:
+            data = json.loads(request.body)
+
+            new_txt = data.get("new_txt")
+
+            if not id or not new_txt:
+                return JsonResponse({"message": "New text is required"}, status= 400)
+
+            req_msg = Message.objects.get(pk=id)
+            req_msg.msg_txt = new_txt
+            req_msg.save()
+            return JsonResponse({"message": "Message updated succesfully"}, status = 200)
+        except DoesNotExist as d:
+            return JsonResponse({"message" : str(d)}, status = 400)
+        except Exception as e:
+            return JsonResponse({"message" : str(e)}, status = 500)
+
+    def delete(self,request,id):
+        try:
+            req_msg = Message.objects.get(pk = id)
+            req_msg.delete()
+            return JsonResponse({"message":"Message deleted successfully"}, status= 204)
+        except DoesNotExist as d:
+            return JsonResponse({"message": str(d)}, status = 400)
+        except Exception as e:
+            return JsonResponse({"message": str(e)}, status = 500)
